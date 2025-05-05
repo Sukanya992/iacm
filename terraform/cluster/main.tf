0d862ddf8b001f7ed5a04a73062cb882cbab605b
Sukanya992 <sukanya09200206@gmail.com>
2025-05-05 12:42:02 +0530

    Update main.tf

--- terraform/cluster/main.tf
+++ terraform/cluster/main.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project     = "plated-epigram-452709-h6"
-  zone      = "us-cental1-c"
-}
-
 resource "google_container_cluster" "primary" {
   name     = "my-k8s-cluster"
   location = "us-centra1-c"
