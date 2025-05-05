03cdb079f5af3473eebc48c18848a554dac20db3
Sukanya992 <sukanya09200206@gmail.com>
2025-05-05 12:29:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,5 @@ module "cluster" {
 module "delegate" {
   source = "./delegate"
 
-  depends_on = [module.cluster] # Ensure delegate installs after cluster
-  harness_account_id  = var.harness_account_id
-  harness_delegate_token = var.harness_delegate_token
+  depends_on = [module.cluster]  # This ensures delegate runs after cluster
 }
