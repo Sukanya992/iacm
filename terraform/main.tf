af5c02fc7522d04ff7342e5ad2a6d2be6d7ddd0a
Sukanya992 <sukanya09200206@gmail.com>
2025-05-05 15:11:17 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  zone = var.region
+  project = "plated-epigram-452709-h6"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  deletion_protection = true
+
+  initial_node_count = var.node_count
+
+  node_config {
+    machine_type = var.node_machine_type
+    disk_size_gb = 20
+  }
+
+  remove_default_node_pool = false
+}
