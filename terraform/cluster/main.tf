f11807f0a7f27c5af70e22ada49a977132417a9f
Sukanya992 <sukanya09200206@gmail.com>
2025-05-05 15:09:02 +0530

    Delete terraform directory

--- terraform/cluster/main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_container_cluster" "primary" {
-  name     = "my-k8s-cluster"
-  zone = "us-central1-c"
-  initial_node_count = 3
-
-  node_config {
-    machine_type = "e2-medium"
-  }
-}
